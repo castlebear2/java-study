b88564cd3ee787eb1d5143ba1738a4aba65dd07c
JSW <JSW@DESKTOP-JSW>
2022-12-21 17:33:01 +0900

    Practice01 _05번 문제 다시 품(맞춤)

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -6,18 +6,28 @@ public class Prob5 {
 
 		for (int i = 3; i <= 99; i++) {
 			int[] arr = new int[2];
-
+			int ans = 0;
 			arr[0] = i / 10;
 			arr[1] = i % 10;
 
-			if ((arr[0] % 3 == 0 && arr[0] !=0 ) || (arr[1] % 3 == 0 && arr[1] !=0)) {
-				if (arr[0] == 0) {
+			if (arr[0] % 3 == 0 && arr[0] != 0)
+				ans++;
+			if (arr[1] % 3 == 0 && arr[1] != 0) 
+				ans++;
+			
+			if (ans == 2) {
+				System.out.println(arr[0]+""+arr[1]+"짝짝");
+			}
+			if (ans == 1) {
+				if(arr[0]==0){
 					System.out.println(arr[1]+"짝");
-				} else {
+				}else {
 					System.out.println(arr[0]+""+arr[1]+"짝");
 				}
 			}
+			
+		
 		}
 
-	}
+}
 }
