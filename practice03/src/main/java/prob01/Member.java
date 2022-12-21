51835c898c9358a3a484f2a7d6070881a34161f3
JSW <JSW@DESKTOP-JSW>
2022-12-21 20:49:38 +0900

    Practice03 _ 01번 문제

---
+++ practice03/src/main/java/prob01/Member.java
@@ -0,0 +1,26 @@
+package prob01;
+
+public class Member {
+	private String id;
+	private int point;
+	private String name;
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getPoint() {
+		return point;
+	}
+}
