f548ce1dff24ab67090d72edcb713a6a500336e0
JSW <JSW@DESKTOP-JSW>
2022-12-21 10:07:16 +0900

    practice01 _ 01번 문제 풀이.

---
+++ practice01/src/main/java/prob1/Prob1.java
@@ -0,0 +1,15 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		/* 코드 작성 */
+		
+		int number = scanner.nextInt();
+		if(number%3==0) System.out.println("3의 배수입니다.");
+		scanner.close();
+	}
+}
