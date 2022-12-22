34771b16bfc9fac9447dbd93729cb89e046edff8
JSW <JSW@DESKTOP-JSW>
2022-12-22 16:59:49 +0900

    Practice04 _03번 문제.

---
+++ practice04/src/main/java/prob03/Tank.java
@@ -0,0 +1,10 @@
+package prob03;
+
+public class Tank extends Unit{
+	
+	void changeMode() {
+		/* 공격모드를 변환한다. */
+		int num = x;
+		
+	}
+}
---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,18 @@
+package prob03;
+
+public class Unit {
+	
+	public int x;
+	public int y;
+	
+	public Unit() {
+
+	}
+	
+	void move (int x, int y) {
+		
+	}
+	void stop () {
+		
+	}
+}
