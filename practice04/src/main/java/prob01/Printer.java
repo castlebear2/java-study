14edd9b916e2629507f54c2b8be1149ba3b352c8
JSW <JSW@DESKTOP-JSW>
2022-12-22 15:03:24 +0900

    Practice04 _ 01번 문제풀이.

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,17 @@
+package prob01;
+
+public class Printer {
+	
+	public void println(String str) {
+		System.out.println(str);
+	}
+	public void println(int num) {
+		System.out.println(num);
+	}
+	public void println(double num) {
+		System.out.println(num);
+	}
+	public void println(boolean b) {
+		System.out.println(b);
+	}
+}
