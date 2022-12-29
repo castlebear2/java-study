5e32a638e4ff714553da1a6917b0300a13514017
JSW <JSW@DESKTOP-JSW>
2022-12-29 19:53:32 +0900

    RequestHandler _404 및 400 해결 완료.

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,13 +1,16 @@
 package httpd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
 
 	public RequestHandler(Socket socket) {
@@ -19,37 +22,55 @@ public class RequestHandler extends Thread {
 		try {
 			// get IOStream
 			OutputStream outputStream = socket.getOutputStream();
+			// 
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
-			
+			// 
 			// logging Remote Host IP Address & Port
 			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			// 캐스팅해서 주소 받아옴
 			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
 					+ inetSocketAddress.getPort());
 
-			
-			
-			while(true) {
-				String line =  br.readLine();
-				
+			String request = null;
+			while (true) {
+				String line = br.readLine();
+
 				// 브라우저 연결 끊으면
-				if(line == null) {
+				if (line == null) {
 					break;
 				}
-				
+
 				// SimpleHttpServer는 요청의 헤더만 처리한다.
-				if("".equals(line)) {
+				if ("".equals(line)) {
 					break;
 				}
-				
-				consoleLog(line);
+
+				// 요청 헤더의 첫 번째 라인만 읽음
+				if (request == null) {
+					request = line;
+					break;
+				}
 			}
-			
+			// 요청 처리
+			// consoleLog(request);
+			String[] tokens = request.split(" ");
+			if ("GET".equals(tokens[0])) {
+				consoleLog(request);
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				// methods: GET, POST, PUT, DELETE, HEAD, CONNECT
+				// SimpleHttppServer 에서는 무시( 400 Bad Request) >> 아무 응답 안하는 게 아니다. response는 항상 해줘야 한다. 하나다. 짝이다!
+				
+				// 과제 : 이거 구현하기!!!
+				response400Error(outputStream, tokens[2]);
+			}
+
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
-			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
-			outputStream.write("\r\n".getBytes());
-			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
+//			outputStream.write("HTTP/1.1 200 OK\r\n".getBytes("UTF-8"));
+//			outputStream.write("Content-Type:text/html; charset=utf-8\r\n".getBytes("UTF-8"));
+//			outputStream.write("\r\n".getBytes());
+//			outputStream.write("<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes("UTF-8"));
 
 		} catch (Exception ex) {
 			consoleLog("error:" + ex);
@@ -64,6 +85,74 @@ public class RequestHandler extends Thread {
 				consoleLog("error:" + ex);
 			}
 		}
+	}
+
+
+	private void responseStaticResource(
+			OutputStream outputStream, 
+			String url, 
+			String protocol) throws IOException {
+			// default(welcome) file set
+			if("/".equals(url)) {
+				url = "/index.html";
+			}
+		
+			File file = new File(DOCUMENT_ROOT + url);
+			if(!file.exists()) {
+				response404Error(outputStream, protocol);
+				return;
+			}
+		
+			// nio
+			byte[] body = Files.readAllBytes(file.toPath());
+			String contentType = Files.probeContentType(file.toPath());
+			
+			// 응답
+			outputStream.write((protocol + " 200 OK \r\n").getBytes("UTF-8"));
+			outputStream.write(("Content-Type:"+  contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+			outputStream.write("\r\n".getBytes());
+			outputStream.write(body);
+
+	}
+
+	private void response400Error(OutputStream outputStream, String protocol) throws IOException{
+		// HTTP/1.1 400 Bad Request
+		// Content-Type:.....
+		// \r\n
+		// .....
+
+	
+		File file = new File(DOCUMENT_ROOT + "/error/400.html");
+	
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		
+		// 응답
+		outputStream.write((protocol + " 400 Bad Request \r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:"+  contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
+	}
+	
+	 
+	private void response404Error(OutputStream outputStream, String protocol) throws IOException{
+		// HTTP/1.1 404 Not Found 
+		// Content-Type:.....
+		// \r\n
+		// .....
+		
+		File file = new File(DOCUMENT_ROOT + "/error/404.html");
+		
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		
+		// 응답
+		outputStream.write((protocol + " 404 Not Found \r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:"+  contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
 	}
 
 	public void consoleLog(String message) {
