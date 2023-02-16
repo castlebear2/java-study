074651dfdce4382a7f55f2a5f641487e4c6533ee
castlebear2 <tjddnd5729@gmail.com>
2023-02-16 13:22:35 +0900

    java practice04_1

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,16 +2,37 @@ package prob01;
 
 public class Printer {
 	
-	public void println(String str) {
-		System.out.println(str);
+	
+	public void println(int i) {
+		System.out.println(i);
 	}
-	public void println(int num) {
-		System.out.println(num);
-	}
-	public void println(double num) {
-		System.out.println(num);
-	}
+	
 	public void println(boolean b) {
 		System.out.println(b);
 	}
+	
+	public void println(double d) {
+		System.out.println(d);
+	}
+	
+	public void println(String s) {
+		System.out.println(s);
+	}
+//	public <T> void println(T t) {
+//		System.out.println(t);
+//	}
+//	
+//	public int sum(Integer... nums) {
+//		int s = 0;
+//		for(Integer n:nums) {
+//			s+=n;
+//		}
+//		
+//		return s;
+//	}
+//	public <T> void println(T... ts) {
+//		for(T t:ts) {
+//			System.out.println(t);
+//		}
+//	}
 }
