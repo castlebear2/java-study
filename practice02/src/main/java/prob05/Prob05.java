389568692a9f69321c22ccb4783294f2d1ed5cf7
castlebear2 <tjddnd5729@gmail.com>
2023-02-16 14:08:07 +0900

    java practice04 _05

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -6,35 +6,26 @@ import java.util.Scanner;
 public class Prob05 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		// 정답 램덤하게 만들기
-		Random random = new Random();
-		int correctNumber = random.nextInt(100) + 1;
-		while (true) {
+		Scanner scanner = new Scanner( System.in );
 
+		while( true ) {
+			
 			/* 게임 작성 */
 
-			// 새 게임 여부 확인하기
-			System.out.println("맞춰보세요 한 번");
-			int guessNum = scanner.nextInt();
-			if (guessNum == correctNumber) {
-				System.out.println("맞췄습니다!!!");
-				break;
-			} else {
-				if (guessNum > correctNumber) {
-					System.out.println("DOWN");
-				}
-				else {
-					System.out.println("UP");
-				}
-			}
-			System.out.print("다시 하겠습니까(y/n)>>");
+			// 정답 램덤하게 만들기
+			Random random = new Random();
+			int correctNumber = random.nextInt( 100 ) + 1;
+			System.out.println(correctNumber);
+
+			
+			//새 게임 여부 확인하기
+			System.out.print( "다시 하겠습니까(y/n)>>" );
 			String answer = scanner.next();
-			if ("y".equals(answer) == false) {
+			if( "y".equals( answer ) == false ) {
 				break;
 			}
 		}
-
+		
 		scanner.close();
 	}
 
