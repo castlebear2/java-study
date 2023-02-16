6556d1bfea75dce8e4094d4e188500a3cfba3779
castlebear2 <tjddnd5729@gmail.com>
2023-02-16 14:39:24 +0900

    java practice05-01

--- practice05/src/main/java/prob1/Sort.java
+++ practice05/src/main/java/prob1/Sort.java
@@ -16,16 +16,17 @@ public class Sort {
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
 		//
-		int tmp;
-		for(int i=0; i<array.length-1; i++) {
-			for(int j=0; j<array.length-1-i; j++) {
-				if(array[j] > array[j+1]) {
-					tmp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = tmp;
+		for (int i = 0 ; i < count-1; i++) {
+			for(int j=count-1-i; j>0; j--) {
+				int tmp;
+				if(array[j] > array[j-1]) {
+					tmp = array[j-1];
+					array[j-1] = array[j];
+					array[j] = tmp;
 				}
 			}
 		}
+
 		
 		// 결과 출력
 		System.out.println( "\nAfter Sort." );
